7eb4a85be87ee203cc10e4d6e4b27c1f694ca9a3
Mykola Klievanskyi <nikolai.klevanski@gmail.com>
2023-03-14 22:28:25 +0200

    feat: add footer and home layouts

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import Header from './Header'
+import Footer from './Footer'
+import Home from './Home'
 
 function App() {
   return (
-    <Header/>
+    <>
+      <Header/>
+      <Home/>
+      <Footer/>
+    </>
   )
 }
 
