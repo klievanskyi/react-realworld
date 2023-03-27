e6d0c88722e5ade23d6ab7b460d09622d6cdb77c
Mykola Klievanskyi <nikolai.klevanski@gmail.com>
2023-03-27 09:56:07 +0300

    feat: add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import Home from './components/Pages/Home'
+import Main from './components/Layouts/Main'
+import Login from "./components/Pages/Login";
+import Register from "./components/Pages/Register";
+import ErrorPage from "./components/Pages/Error-page";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main/>,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home/>
+      },
+      {
+        path: "/login",
+        element: <Login/>
+      },
+      {
+        path: "/register",
+        element: <Register/>
+      }
+    ]
+  }
+])
 
 function App() {
   return (
-    <>
-      <Header/>
-      <Home/>
-      <Footer/>
-    </>
+    <RouterProvider router={router} />
   )
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,18 @@
+import { Link } from 'react-router-dom'
+
 function Header() {
   return (
     <nav className="navbar navbar-light">
       <div className="container">
-        <a className="navbar-brand" href="index.html">
+        <Link className="navbar-brand" to="/a">
           conduit
-        </a>
+        </Link>
         <ul className="nav navbar-nav pull-xs-right">
           <li className="nav-item">
             {/* Add "active" class when you're on that page"  */}
-            <a className="nav-link active" href="">
+            <Link className="nav-link active" to="/">
               Home
-            </a>
+            </Link>
           </li>
           <li className="nav-item">
             <a className="nav-link" href="">
@@ -25,14 +27,14 @@ function Header() {
             </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="">
+            <Link className="nav-link" to={`/login`}>
               Sign in
-            </a>
+            </Link>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="">
+            <Link className="nav-link" to={`/register`}>
               Sign up
-            </a>
+            </Link>
           </li>
         </ul>
       </div>
