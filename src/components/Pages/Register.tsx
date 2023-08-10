d58238ba4b02e97a36afb4de158e86c25837c360
Mykola Klievanskyi <nikolai.klevanski@gmail.com>
2023-08-10 13:19:20 +0300

    rethink

--- src/components/Pages/Register.tsx
+++ src/components/Pages/Register.tsx
@@ -1,4 +1,36 @@
+import { useState } from "react";
+
 function Register() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const formData = {
+    username: "",
+    email: "",
+    password: ""
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    formData.username = username;
+    formData.email = email;
+    formData.password = password;
+
+    async function fetchData() {
+      const response = await fetch("https://api.realworld.io/api/users", {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({user: formData})
+      })
+      const data = await response.json();
+      console.log(data);
+    }
+    fetchData();
+    console.log(formData);
+
+  }
+
   return (
     <div className="auth-page">
       <div className="container page">
@@ -13,12 +45,13 @@ function Register() {
               <li>That email is already taken</li>
             </ul>
 
-            <form>
+            <form method="post" onSubmit={handleSubmit}>
               <fieldset className="form-group">
                 <input
                   className="form-control form-control-lg"
                   type="text"
                   placeholder="Your Name"
+                  onChange={e => setUsername(e.target.value)}
                 />
               </fieldset>
               <fieldset className="form-group">
@@ -26,6 +59,7 @@ function Register() {
                   className="form-control form-control-lg"
                   type="text"
                   placeholder="Email"
+                  onChange={e => setEmail(e.target.value)}
                 />
               </fieldset>
               <fieldset className="form-group">
@@ -33,6 +67,7 @@ function Register() {
                   className="form-control form-control-lg"
                   type="password"
                   placeholder="Password"
+                  onChange={e => setPassword(e.target.value)}
                 />
               </fieldset>
               <button className="btn btn-lg btn-primary pull-xs-right">
